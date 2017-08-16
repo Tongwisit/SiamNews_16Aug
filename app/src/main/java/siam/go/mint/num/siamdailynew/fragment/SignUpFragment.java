0e584bc3bfa249e94780f7e69aca1e29748d4795
test <day_12382@gmail.com>
2017-08-16 13:33:11 +0700

    Back Controllor Finish

--- app/src/main/java/siam/go/mint/num/siamdailynew/fragment/SignUpFragment.java
+++ app/src/main/java/siam/go/mint/num/siamdailynew/fragment/SignUpFragment.java
@@ -1,11 +1,14 @@
 package siam.go.mint.num.siamdailynew.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import siam.go.mint.num.siamdailynew.R;
 
@@ -16,8 +19,31 @@ import siam.go.mint.num.siamdailynew.R;
 public class SignUpFragment extends Fragment{
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
         return view;
+    } // onCreate View
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // Back Controllor
+        backControllor();
+
+    }
+
+    private void backControllor() {
+        ImageView imageView = getView().findViewById(R.id.imvBack);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("16Augv1", "ClickOk");
+                getActivity().getSupportFragmentManager()
+                        .popBackStack();
+            }
+        });
     }
 }   //Main Class
